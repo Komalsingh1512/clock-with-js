03641dbc0e4ea129950f3391c7db09d80e570385
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2023-02-10 10:46:52 +0530

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+setInterval(setDate, 1000);
+
 const secondHand = document.querySelector('.second-hand');
   const minsHand = document.querySelector('.min-hand');
   const hourHand = document.querySelector('.hour-hand');
@@ -6,18 +8,15 @@ const secondHand = document.querySelector('.second-hand');
     const now = new Date();
 
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360);
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6);
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30);
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
   }
-
-  setInterval(setDate, 1000);
-
   setDate();
